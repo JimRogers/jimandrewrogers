6cf5788f7eeb42d527a1a84e69d2f7c00d9c5b02
Jim <weareonegravity@gmail.com>
2021-05-11 21:45:33 +0100

    Add further styling to songwriting sessions

--- contact.php
+++ contact.php
@@ -16,18 +16,7 @@ $headers .= 'Reply-To: '.$email."\r\n";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
-if ($mail_status) { ?>
- <script language="javascript" type="text/javascript">
-  alert('Thank you for the message. We will contact you shortly.');
-  window.location = 'https://www.jimandrewrogers.com/songwriting-sessions/';
- </script>
-<?php
-}
-else { ?>
- <script language="javascript" type="text/javascript">
-  alert('Message sending failed. Please, send an email to jimandrewrogers@gmail.com');
-  window.location = 'https://www.jimandrewrogers.com/songwriting-sessions/';
- </script>
-<?php
-}
+echo "Thank You!";
 ?>
+
+
