905bca1ad0ff0f8059436b6bed9b0c79395ba668
Jim <weareonegravity@gmail.com>
2021-05-11 21:40:13 +0100

    Add adjustments to form

--- contact.php
+++ contact.php
@@ -2,18 +2,17 @@
 
 
 <?php
-
-$field_name = $_POST['Name'];
-$field_email = $_POST['E-Mail'];
-$field_message = $_POST['Message'];
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['message'];
 $mail_to = 'jimandrewrogers@gmail.com';
 $subject = '#Message# '.$field_name;
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
 $body_message .= 'Message: '.$field_message;
 
-$headers = 'From: '.$E-Mail."\r\n";
-$headers .= 'Reply-To: '.$E-Mail."\r\n";
+$headers = 'From: '.$email."\r\n";
+$headers .= 'Reply-To: '.$email."\r\n";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
